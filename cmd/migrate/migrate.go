1079f74257a561119e1bc9525b76d28a52f54b84
iki-rumondor <ikirumondor07@gmail.com>
2024-01-23 11:28:01 +0800

    feat: membuat command seeder

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -16,6 +16,10 @@ func ReadTerminal(db *gorm.DB, args []string) {
 		if err := freshDatabase(db); err != nil {
 			log.Fatal(err.Error())
 		}
+	case args[1] == "seed":
+		if err := seederData(db); err != nil {
+			log.Fatal(err.Error())
+		}
 	default:
 		fmt.Println("Hello")
 	}
@@ -42,6 +46,89 @@ func freshDatabase(db *gorm.DB) error {
 		Uuid:   uuid.NewString(),
 		UserID: 1,
 	})
+
+	return nil
+}
+
+func seederData(db *gorm.DB) error {
+
+	userData := domain.User{
+		Nama:     "John Doe",
+		Username: "johndoess",
+		Password: "123",
+	}
+
+	if err := db.Create(&userData).Error; err != nil {
+		return err
+	}
+
+	teacher := domain.Teacher{
+		Uuid:          uuid.NewString(),
+		Nuptk:         "1234567890",
+		StatusPegawai: "AKTIF",
+		Nip:           "98765432122",
+		JK:            "Laki-laki",
+		TempatLahir:   "Jakarta",
+		TanggalLahir:  "1990-01-01",
+		NoHp:          "081234567890",
+		Jabatan:       "Guru",
+		TotalJtm:      "40",
+		Alamat:        "Jl. Contoh No. 123",
+		UserID:        userData.ID,
+	}
+
+	if err := db.Create(&teacher).Error; err != nil {
+		return err
+	}
+
+	class := domain.Class{
+		Uuid:      uuid.NewString(),
+		Name:      "VII-A",
+		TeacherID: teacher.ID,
+	}
+
+	if err := db.Create(&class).Error; err != nil {
+		return err
+	}
+
+	subject := domain.Subject{
+		Uuid:      uuid.NewString(),
+		Name:      "Fisika",
+		TeacherID: teacher.ID,
+	}
+
+	if err := db.Create(&subject).Error; err != nil {
+		return err
+	}
+
+	userStudent := domain.User{
+		Nama:     "John Doe",
+		Username: "student",
+		Password: "123",
+	}
+
+	if err := db.Create(&userStudent).Error; err != nil {
+		return err
+	}
+
+	student := domain.Student{
+		Uuid:         uuid.NewString(),
+		NIS:          "12345678",
+		JK:           "LAKI-LAKI",
+		TempatLahir:  "Gorontalo",
+		TanggalLahir: "2000-01-01",
+		Alamat:       "Alamat Siswa",
+		UserID:       userStudent.ID,
+		ClassID:      class.ID,
+	}
+
+	if err := db.Create(&student).Error; err != nil {
+		return err
+	}
+
+	if err := db.Create(&domain.SchoolYear{Name: "2023"}).Error; err != nil {
+		return err
+	}
 
 	return nil
 }
