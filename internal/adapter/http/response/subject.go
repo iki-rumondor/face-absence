4bfccc7ecb6d0d71b861e32ce24765af7d70e322
iki-rumondor <ikirumondor07@gmail.com>
2024-01-26 23:28:27 +0800

    fix: memperbaiki penamaan response json subjects

--- internal/adapter/http/response/subject.go
+++ internal/adapter/http/response/subject.go
@@ -5,7 +5,7 @@ import "time"
 type SubjectResponse struct {
 	Uuid      string     `json:"uuid"`
 	Name      string     `json:"name"`
-	Teachers   *[]Teacher `json:"teacher"`
+	Teachers  *[]Teacher `json:"teachers"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 }
